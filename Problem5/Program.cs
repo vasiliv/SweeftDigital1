25956b50b4d757757069bc306ef41b54e9fc1fa8
vasiliv <ggo24@yahoo.com>
2021-06-29 22:18:47 +0400

    problem 5 finished

---
+++ Problem5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Problem5
+{
+    class Program
+    {         
+        static void Main(string[] args)
+        {
+            Console.WriteLine(countVariants(5));
+        }                
+        public static int countVariants(int stearsCount)
+        {
+            int a = 1;
+            int b = 2;
+            int c = 0;
+            for (int i = 2; i < stearsCount; i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+            }
+            return c;
+        }
+    }
+}
---
+++ test3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace test3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Fibonacci(10));
+        }
+        public static int Fibonacci(int n)
+        {
+            int a = 0;
+            int b = 1;
+            // In N steps, compute Fibonacci sequence iteratively.
+            for (int i = 0; i < n; i++)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
+        }
+    }
+}
